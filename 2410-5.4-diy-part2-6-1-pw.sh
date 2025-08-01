56f27ac03c991f9e81c240e69b5772fe40ca3db8
P3N99 <145282517+P3N99@users.noreply.github.com>
2025-08-02 01:54:28 +0800

    Update 2410-5.4-diy-part2-6-1-pw.sh

--- 2410-5.4-diy-part2-6-1-pw.sh
+++ 2410-5.4-diy-part2-6-1-pw.sh
@@ -22,7 +22,7 @@ git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/l
 
 
 # Modify hostname
-sed -i 's/ImmortalWrt/ImmortalWrt-24.10-5.4-PW-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/P3N99-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
 
 # Modify filename, add date prefix
 sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-PW-24.10-5.4-|' include/image.mk
